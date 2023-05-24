868f3772bbf746e2c98a5856f05e357150d9d324
LuanPereira <88207846+LuanPBS98@users.noreply.github.com>
2023-05-24 16:24:22 -0300

    feat: images

--- src/components/fluxoCaixaMain/fluxoCaixaMain.js
+++ src/components/fluxoCaixaMain/fluxoCaixaMain.js
@@ -1,4 +1,7 @@
-import { Box, Card } from "@mui/material";
+import { Box, Card, ImageListItem, Typography } from "@mui/material";
+import DocumentScannerIcon from "@mui/icons-material/DocumentScanner";
+import Image from "mui-image";
+import { fluxoCaixaCard } from "../../utils/fluxoCaixaCards";
 
 export const FluxoCaixaMain = () => {
   return (
@@ -6,18 +9,46 @@ export const FluxoCaixaMain = () => {
       <Card
         sx={{
           width: { xs: "100%", sm: "90%" },
+          height: "900px",
           mx: "auto",
           mt: 2,
         }}
         style={{
-          backgroundColor: "#141414",
           display: "flex",
           flexDirection: "column",
+          background: "#000000",
         }}
       >
-        <Box style={{}}>
-
-        </Box>
+        {fluxoCaixaCard.map((e, index) => (
+          <Card
+            key={index}
+            sx={{ width: "28%", height: "150px" }}
+            style={{
+              background: "#141414",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <Box>
+              <Typography color="#ffffff" style={{ fontSize: "20px" }}>
+                {e.name}
+              </Typography>
+              <Typography style={{ fontSize: "14px", color: "#969696" }}>
+                {e.description}
+              </Typography>
+              <Box display="flex" flexDirection="row">
+                <DocumentScannerIcon sx={{ color: "#969696" }} />
+                <Typography style={{ fontSize: "14px", color: "#4BB543" }}>
+                  {e.value}
+                </Typography>
+              </Box>
+            </Box>
+            <Box>
+              <Image src={e.img} alt="text" />
+            </Box>
+          </Card>
+        ))}
       </Card>
     </>
   );
---
+++ src/utils/fluxoCaixaCards.js
@@ -0,0 +1,3 @@
+export const fluxoCaixaCard = [
+    {name: "Lançamentos diversos", description: "Seu saldo disponível total é de (R$):", img: 'assets/imagemBoy.png"', value: '123.456.789'}
+]
