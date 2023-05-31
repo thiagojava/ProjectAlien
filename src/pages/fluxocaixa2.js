c30f37798711a7a520c30ccd8c7f12a9423956b7
LuanPereira <88207846+LuanPBS98@users.noreply.github.com>
2023-05-31 15:57:08 -0300

    revision

--- src/pages/fluxocaixa2.js
+++ src/pages/fluxocaixa2.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Navbar from "../components/Layout/Navbar";
 import { FluxoCaixaMain2 } from "../components/fluxoCaixaMain2/fluxoCaixaMain2";
 import { BoxCharts } from "../components/fluxoCaixaMain2/boxCharts/BoxCharts";
@@ -6,16 +6,20 @@ import { BoxHistory } from "../components/fluxoCaixaMain2/boxHistoric/boxHistori
 import { TableList } from "../components/table/Table";
 import { tablesFluxoDeCaixa } from "../utils/tables";
 
-
 const FluxoCaixa = () => (
   <>
     <Navbar />
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <FluxoCaixaMain2 />
-        <BoxCharts />
-        <BoxHistory />
-        <TableList heads={tablesFluxoDeCaixa[0].heads} rows={tablesFluxoDeCaixa[1].rows} />
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <FluxoCaixaMain2 />
+          <BoxCharts />
+          <BoxHistory />
+          <TableList
+            heads={tablesFluxoDeCaixa[0].heads}
+            rows={tablesFluxoDeCaixa[1].rows}
+          />
+        </Box>
       </Grid>
     </Grid>
   </>
