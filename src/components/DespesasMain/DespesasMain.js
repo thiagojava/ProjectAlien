6f61285d36f49748899390594c2770a3357d3e4f
LuanPereira <88207846+LuanPBS98@users.noreply.github.com>
2023-05-31 15:11:55 -0300

    feat

--- src/components/DespesasMain/DespesasMain.js
+++ src/components/DespesasMain/DespesasMain.js
@@ -1,5 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { CardBox } from "../card/Index";
+import { SimpleCircleChart } from "../charts/circle/SimpleCircleChart";
+import { headerDespesas } from "../../utils/headersList";
+import { colorsChart } from "../../utils/colors";
+import { MiniCard } from "../card/MiniCard";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
 
 export const DespesasMain = () => {
   return (
@@ -10,9 +15,60 @@ export const DespesasMain = () => {
         marginTop="10px"
         justifyContent="space-between"
       >
-        <CardBox basis="24%"></CardBox>
-        <CardBox basis="24%"></CardBox>
-        <CardBox basis="48%"></CardBox>
+        <CardBox basis="24%">
+          <Box marginTop="70px">
+            <Typography color="#969696" style={{ fontSize: "12px" }}>
+              R$
+            </Typography>
+            <Typography style={{ fontSize: "24px", color: "#ffffff" }}>
+              123.456.789
+            </Typography>
+          </Box>
+        </CardBox>
+        <CardBox basis="24%">
+          <Box marginTop="70px">
+            <Typography color="#969696" style={{ fontSize: "12px" }}>
+              R$
+            </Typography>
+            <Typography style={{ fontSize: "24px", color: "#ffffff" }}>
+              123.456.789
+            </Typography>
+          </Box>
+        </CardBox>
+        <CardBox basis="48%">
+          <Box display="flex" justifyContent="space-around">
+            {headerDespesas.map((e, index) => {
+              const colors = colorsChart[Math.floor(Math.random() * 5)];
+              return (
+                <Box
+                  flexBasis="10%"
+                  key={index}
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                >
+                  <Box key={index} width="80px">
+                    <SimpleCircleChart colors={colors} />
+                  </Box>
+                  <Box
+                    marginBottom="10px"
+                    padding="10px"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                  >
+                    <Typography color="#969696" style={{ fontSize: "12px" }}>
+                      {e}
+                    </Typography>
+                    <Typography style={{ fontSize: "9px", color: "#ffffff" }}>
+                      123.456.789
+                    </Typography>
+                  </Box>
+                </Box>
+              );
+            })}
+          </Box>
+        </CardBox>
       </Box>
       <Box
         display="flex"
@@ -20,8 +76,88 @@ export const DespesasMain = () => {
         marginTop="10px"
         justifyContent="space-between"
       >
-        <CardBox basis="48%"></CardBox>
-        <CardBox basis="48%"></CardBox>
+        <CardBox basis="48%">
+          <Box display="flex" gap="150px">
+            <Box>
+              <Box marginTop="50px">
+                <MiniCard
+                  icon={
+                    <HomeWorkIcon fontSize="large" sx={{ color: "#969696" }} />
+                  }
+                  title="Total acumulado (R$)"
+                />
+                <Box marginTop="30px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de entradas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#008000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+                <Box marginTop="20px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de saídas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#ff0000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box width="200px" display="flex" alignItems="center">
+              <SimpleCircleChart colors={{ back: "#9800f3" }} />
+              <Box>
+                <Typography color="#969696" style={{ fontSize: "12px" }}>
+                  Média
+                </Typography>
+                <Typography style={{ fontSize: "24px", color: "#9800f3" }}>
+                  123.456.789
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </CardBox>
+        <CardBox basis="48%">
+          <Box display="flex" gap="150px">
+            <Box>
+              <Box marginTop="50px">
+                <MiniCard
+                  icon={
+                    <HomeWorkIcon fontSize="large" sx={{ color: "#969696" }} />
+                  }
+                  title="Total acumulado (R$)"
+                />
+                <Box marginTop="30px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de entradas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#008000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+                <Box marginTop="20px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de saídas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#ff0000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box width="200px" display="flex" alignItems="center">
+              <SimpleCircleChart colors={{ back: "#e03616" }} />
+              <Box>
+                <Typography color="#969696" style={{ fontSize: "12px" }}>
+                  Média
+                </Typography>
+                <Typography style={{ fontSize: "24px", color: "#e03616" }}>
+                  123.456.789
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </CardBox>
       </Box>
       <Box
         display="flex"
@@ -29,22 +165,143 @@ export const DespesasMain = () => {
         marginTop="10px"
         justifyContent="space-between"
       >
-        <CardBox basis="48%"></CardBox>
-        <CardBox basis="48%"></CardBox>
+        <CardBox basis="48%">
+          <Box display="flex" gap="150px">
+            <Box>
+              <Box marginTop="50px">
+                <MiniCard
+                  icon={
+                    <HomeWorkIcon fontSize="large" sx={{ color: "#969696" }} />
+                  }
+                  title="Total acumulado (R$)"
+                />
+                <Box marginTop="30px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de entradas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#008000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+                <Box marginTop="20px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de saídas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#ff0000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box width="200px" display="flex" alignItems="center">
+              <SimpleCircleChart colors={{ back: "#E97406" }} />
+              <Box>
+                <Typography color="#969696" style={{ fontSize: "12px" }}>
+                  Média
+                </Typography>
+                <Typography style={{ fontSize: "24px", color: "#E97406" }}>
+                  123.456.789
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </CardBox>
+        <CardBox basis="48%">
+          <Box display="flex" gap="150px">
+            <Box>
+              <Box marginTop="50px">
+                <MiniCard
+                  icon={
+                    <HomeWorkIcon fontSize="large" sx={{ color: "#969696" }} />
+                  }
+                  title="Total acumulado (R$)"
+                />
+                <Box marginTop="30px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de entradas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#008000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+                <Box marginTop="20px">
+                  <Typography color="#969696" style={{ fontSize: "12px" }}>
+                    Total de saídas
+                  </Typography>
+                  <Typography style={{ fontSize: "18px", color: "#ff0000" }}>
+                    123.456.789
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box width="200px" display="flex" alignItems="center">
+              <SimpleCircleChart colors={{ back: "#41d3bd" }} />
+              <Box>
+                <Typography color="#969696" style={{ fontSize: "12px" }}>
+                  Média
+                </Typography>
+                <Typography style={{ fontSize: "24px", color: "#41d3bd" }}>
+                  123.456.789
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </CardBox>
       </Box>
       <Box
         display="flex"
         width="90%"
         marginTop="10px"
+        marginBottom="10px"
         justifyContent="space-between"
       >
-        <CardBox basis="24%"></CardBox>
-        <CardBox basis="12%"></CardBox>
-        <CardBox basis="12%"></CardBox>
-        <CardBox basis="12%"></CardBox>
-        <CardBox basis="12%"></CardBox>
-        <CardBox basis="12%"></CardBox>
-        <CardBox basis="12%"></CardBox>
+        <CardBox basis="24%">
+          <Box display="flex" gap="50px">
+            <Box>
+              <Box marginTop="50px">
+                <MiniCard
+                  icon={
+                    <HomeWorkIcon fontSize="large" sx={{ color: "#F0E032" }} />
+                  }
+                  title="Total acumulado (R$)"
+                />
+              </Box>
+            </Box>
+            <Box width="150px" display="flex" alignItems="center">
+              <SimpleCircleChart colors={{ back: "#F0E032" }} />
+            </Box>
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#E97406" }} />
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#791e94" }} />
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#008000" }} />
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#9800f3" }} />
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#e03616" }} />
+          </Box>
+        </CardBox>
+        <CardBox basis="12%">
+          <Box width="150px" display="flex" justifyContent="center">
+            <SimpleCircleChart colors={{ back: "#791e94" }} />
+          </Box>
+        </CardBox>
       </Box>
     </>
   );
--- src/utils/headersList.js
+++ src/utils/headersList.js
@@ -16,38 +16,46 @@ export const headerListInvestimento = [
 ];
 
 export const headerListGestor = [
-    {
-      top: ["Faturamento", "Investida", "Start-up", "Trading"],
-      bottom: [
-        "Renda fixa",
-        "Retorno variável",
-        "Cryptomoedas",
-        "Volatilidade",
-        "Cashflow",
-        "Patrimônio",
-        "Capital contratado",
-        "Juros & multas",
-        "Dívidas",
-      ],
-    },
-  ];
+  {
+    top: ["Faturamento", "Investida", "Start-up", "Trading"],
+    bottom: [
+      "Renda fixa",
+      "Retorno variável",
+      "Cryptomoedas",
+      "Volatilidade",
+      "Cashflow",
+      "Patrimônio",
+      "Capital contratado",
+      "Juros & multas",
+      "Dívidas",
+    ],
+  },
+];
 
-  export const headerListDRE = [
-    {
-      top: ["1 Trimestre", "2 Trimestre", "3 Trimestre", "4 Trimestre"],
-      bottom: [
-        "Jan - 2023",
-        "Fev - 2023",
-        "Mar - 2023",
-        "Abr - 2023",
-        "Mai - 2023",
-        "Jun - 2023",
-        "Jul - 2023",
-        "Ago - 2023",
-        "Set - 2023",
-        "Out - 2023",
-        "Nov - 2023",
-        "Dez - 2023",
-      ],
-    },
-  ];
+export const headerListDRE = [
+  {
+    top: ["1 Trimestre", "2 Trimestre", "3 Trimestre", "4 Trimestre"],
+    bottom: [
+      "Jan - 2023",
+      "Fev - 2023",
+      "Mar - 2023",
+      "Abr - 2023",
+      "Mai - 2023",
+      "Jun - 2023",
+      "Jul - 2023",
+      "Ago - 2023",
+      "Set - 2023",
+      "Out - 2023",
+      "Nov - 2023",
+      "Dez - 2023",
+    ],
+  },
+];
+
+export const headerDespesas = [
+  "Sua meta hoje (R$)",
+  "Lazer",
+  "Beleza",
+  "Esporte",
+  "Estilo"
+];
